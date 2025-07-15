67201ea54396964dacceedc373607b4d18709dff
Diego-jpeg-27 <diego.developti@gmail.com>
2025-07-14 21:33:35 -0300

    index.php + estrutura do projeto

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <title>CRUD-Clinic-IFPE</title>
+</head>
+
+<body>
+
+<h2>Formulário de Acesso</h2>
+
+<a href="registrar_consulta.php">
+  <button type="button">Registrar Consulta</button>
+</a><br>
+
+<br><a href="cadastrar_paciente.php">
+  <button type="button">Cadastrar Paciente</button>
+</a><br>
+
+<br><a href="cadastrar_medico.php">
+  <button type="button">Cadastrar Médico</button>
+</a><br>
+
+ </body>
+</html>
