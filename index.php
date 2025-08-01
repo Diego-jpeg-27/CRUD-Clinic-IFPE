0bc882c5965145a3b5523071f92319baf4f0e07a
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-08-01 18:34:15 -0300

    feat: Reestruturação do formulário + Redirecionamento
    Reestruturação do formulário + adicionado botão de cadastre-se na tela de login

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
   <meta charset="UTF-8">
   <title>Login - CRUD Clinic IFPE</title>
   <link rel="stylesheet" href="assets/css/index.css">
-
 </head>
  <body>
-   <form action="auth/login.php" method="POST">
-   <label for="usuario">Usuário:</label><br>
-   <input type="text" name="usuario" required><br><br>
 
-   <label for="senha">Senha:</label><br>
-   <input type="password" name="senha" required><br><br>
+  <form action="auth/login.php" method="POST">
+    <label for="usuario">Usuário:</label><br>
+    <input type="text" name="usuario" required>
 
-   <input type="submit" value="Entrar">
+    <label for="senha">Senha:</label><br>
+    <input type="password" name="senha" required>
+
+    <input type="submit" value="Entrar">
+
+    <a href="auth/cadastrar_usuario.php" class="botao-link">Cadastre-se</a>
   </form>
-
  </body>
 </html>
