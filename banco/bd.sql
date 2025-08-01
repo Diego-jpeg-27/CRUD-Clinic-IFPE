12057813c42ad83609602551ca9a8bb15578a5f0
Diego-jpeg-27 <diego.developTI@gmail.com>
2025-08-01 18:22:51 -0300

    feat: criar tabela de usuários no banco de dados para autenticação
    add nova tabela usuario no bando com a retirada do login admin fixo.

--- banco/bd.sql
+++ banco/bd.sql
@@ -37,3 +37,10 @@ CREATE TABLE consulta (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+-- Tabela de Usuários
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
